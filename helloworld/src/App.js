5161d94766982e682b59cdecf90b99b0f559fd78
sabina <sabieena>
2020-07-31 11:18:12 +0545

    use of react create element

--- helloworld/src/App.js
+++ helloworld/src/App.js
@@ -1,16 +1,19 @@
 
-import React from "react";
+import React, { Component } from "react";
 import Greet from './component/Func';
 import Welcome from './component/Welcome';
+import Hello from'./component/Hello';
 
- const Sabina = () => {
-  return(
-    <div>
-      Hello sabina
-      <Greet />
-      <Welcome />
-    </div>
-  )
+class App extends Component{
+  render(){
+    return(
+      <div className="App">
+        <Welcome/>
+        <Greet/>
+        <Hello/>
+      </div>
+    )
+  }
 }
 
-export default Sabina;
+export default App;
