5e9fe9f26ab87c549265ea552e9507baabe8e102
Sabieena <sabina.bhattrai2015@gmail.com>
2021-02-26 14:02:34 +0545

    change

--- helloworld/src/component/Count.js
+++ helloworld/src/component/Count.js
@@ -4,7 +4,9 @@ function Count() {
     const [count, setCount] = useState(0);
     return (
         <div>
+            <h1>
             <p>You clicked {count} times</p>
+            </h1>
       <button onClick={() => setCount(count + 1)}>
         Click me
       </button>
